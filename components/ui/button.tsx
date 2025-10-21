88ebab34620517148068474c6689724baca084b4
muradpm <abdulkadyr0v@yandex.ru>
2025-10-21 21:00:07 +0300

    fix: update dependencies

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -52,7 +52,7 @@ function Button({
 		<Comp
 			data-slot="button"
 			className={cn(buttonVariants({ variant, size, className }))}
-			{...props}
+			{...(asChild ? (props as any) : props)}
 		/>
 	);
 }
